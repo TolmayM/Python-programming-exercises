368a744ca93212f9e2e9610e515837603b67e9dd
TolmayM <88318635+TolmayM@users.noreply.github.com>
2022-08-10 16:23:44 +0200

    Initial commit

---
+++ python_functions.py
@@ -0,0 +1,20 @@
+def say_hello():
+    print("hello")
+
+say_hello()
+
+
+def say_hello(name):
+    print(f"hello {name}")
+
+say_hello("Julie")
+
+def describe_yourself(name,age):
+    print(f"My name is {name}. My age is {age}")
+
+describe_yourself("Madelein", 20)
+
+def square(number, power):
+    print(pow(number,power))
+
+square(2,1);
